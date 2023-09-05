0382d0deff32ce6f247a744a17c9a773a4516652
Nino Londaridze <londaridzenino7@gmail.com>
2023-09-06 01:26:20 +0400

    Sep 5 V1

---
+++ JS/section-switch.js
@@ -0,0 +1,36 @@
+let story = document.getElementById('story');
+let storyLine = document.getElementById('story-horizontal-line');
+
+let mission = document.getElementById('mission');
+let missionLine = document.getElementById('mission-horizontal-line');
+
+let vission = document.getElementById('vission');
+let vissionLine = document.getElementById('vission-horizontal-line');
+
+story.addEventListener('click', function() {
+    storyLine.style.visibility = 'hidden';
+    missionLine.style.visibility = 'visible';
+    vissionLine.style.visibility = 'visible';
+    story.classList.add('active');
+    mission.classList.remove('active');
+    vission.classList.remove('active');
+})
+
+mission.addEventListener('click', function() {
+    missionLine.style.visibility = 'hidden';
+    storyLine.style.visibility = 'visible';
+    vissionLine.style.visibility = 'visible';
+    story.classList.remove('active');
+    mission.classList.add('active');
+    story.classList.remove('active');
+})
+
+
+vission.addEventListener('click', function() {
+    storyLine.style.visibility = 'visible';
+    missionLine.style.visibility = 'visible';
+    vissionLine.style.visibility = 'hidden';
+    story.classList.remove('active');
+    mission.classList.remove('active');
+    vission.classList.add('active');
+})
